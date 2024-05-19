a47006902ab86c0a5a1d958197d4854fab9a9834
sharmivijay23 <sharmivijay94@gmail.com>
2024-05-18 20:39:03 -0700

    setting up vscode files

--- web/src/containers/QuoteContainer.tsx
+++ web/src/containers/QuoteContainer.tsx
@@ -1,28 +1,15 @@
 import useAppStore from "../stores/app-store";
 
 const QuoteContainer = () => {
-
-    const { username, quoteStore, weekDayQuotes } = useAppStore();
-
-    const currentTime = new Date();
+    const { username, quoteStore } = useAppStore();
 
     return (
-        <div>
-            <div className="flex flex-col justify-end items-end text-white p-14">
-                <div className=" flex text-2xl max-w-xl">
-                    "{quoteStore.quote}"
-                </div>
-                <div className="text-xl max-w-sm pr-5">
-                    - {quoteStore.author}
-                </div>
+        <div className="flex flex-col justify-end items-end text-white m-14">
+            <div className=" flex text-2xl max-w-xl">
+                "{quoteStore.quote}"
             </div>
-            <div className="flex flex-col justify-center items-center mt-32">
-                <div className=" text-white text-3xl ">
-                    {weekDayQuotes.stat1}, {username}! {weekDayQuotes.stat2}
-                </div>
-                <div className="flex max-w-md backdrop-brightness-150 backdrop-blur-md p-6 py-6 mt-3 rounded-lg text-7xl">
-                    {currentTime.getHours().toString().padStart(2, '0') + ":" + currentTime.getMinutes().toString().padStart(2, '0')}
-                </div>
+            <div className="text-xl max-w-sm pr-5">
+                - {quoteStore.author}
             </div>
         </div>
     )
